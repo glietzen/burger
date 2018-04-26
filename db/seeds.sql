e67115a769f188be621d6d83e0d9866f3ab32740
glietzen@gmail.com <glietzen@gmail.com>
2018-04-26 09:26:32 -0500

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steakhouse Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pork Burger', false);
