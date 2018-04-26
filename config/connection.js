8ae8e5263d6452c2f16d5885e1dd56b93fc5d057
glietzen@gmail.com <glietzen@gmail.com>
2018-04-26 16:36:43 -0500

    added heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "garrett",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = myql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "garrett",
+        database: "burgers_db"
+    });
+}
 
 connection.connect((err) => {
     if (err) {
