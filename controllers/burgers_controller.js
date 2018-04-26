da13c67461e2c6b9d894d6501c0fd8a6d6339714
glietzen@gmail.com <glietzen@gmail.com>
2018-04-26 10:19:06 -0500

    controller setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const burger = require('./../models/burger');
+
+let router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burgers', (req,res) => {
+    burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    (result) => {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    let condition = `id = ${req.params.id}`;
+    console.log(`Condition = ${condition}`);
+    burger.updateOne(
+        {devoured: req.body.devoured},
+        condition,
+        (result) => {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    );
+});
